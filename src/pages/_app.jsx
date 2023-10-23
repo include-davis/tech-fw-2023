eb5c25bb8c12d413827ea75f95616108bf0f4310
Ryan <ryyu444@gmail.com>
2023-10-22 19:13:18 -0700

    boilerplate and eslint

---
+++ src/pages/_app.jsx
@@ -0,0 +1,16 @@
+import { Inter } from "next/font/google";
+import "../styles/globals.scss";
+
+const inter = Inter({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div className={`${inter.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default function Index() {
+  return (
+    <div>
+      <h1>
+        hello
+        <div>yes</div>
+        <img src="" />
+      </h1>
+    </div>
+  );
+}
