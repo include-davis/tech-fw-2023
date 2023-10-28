b331063f03fae223bccbda7c4cb395b8377e18c1
naomi zhao <naomizhao@naomis-MacBook-Pro-3.local>
2023-10-28 13:31:47 -0700

    created new branch

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import styles from '@/styles/components/personal/about.module.scss'
 
 export default function About() {
     return (
-      <div>about</div>
+      <div className={styles.outer}>
+        content
+        <div className={styles.inner}>
+          more content
+        </div>
+      </div>
     )
 }
