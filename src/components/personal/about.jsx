b51b19c4b96996cf1b63d3ec9f4e6813544e4a28
Vivica Tran <viamtran@ucsc.edu>
2023-10-28 13:33:18 -0700

    new branch

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import styles from'@/styles/components/personal/about.module.css';
 
 export default function About() {
     return (
-      <div>about</div>
+      <div classname={styles.outer}>
+       about
+      </div>
     )
 }
