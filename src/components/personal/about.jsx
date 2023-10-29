aae190b3c2ca6ab874421a4201796dc60be0458c
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2023-10-28 19:41:45 -0700

    Added about me info to personal page

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import styles from '../../styles/components/personal/about.module.scss';
 
 export default function About() {
     return (
-      <div>Hello, my name is Sreya!</div>
+      <div>
+        <p className={styles.about} >I am a 2nd year computer science & Engineering student at UCD, who would love to utilize my learned skills to make meaningful/innovative projects that can help society! In my spare time, I love arts like crochet and baking. Looking forward to connecting with you :)</p>  
+      </div>
     )
 }
