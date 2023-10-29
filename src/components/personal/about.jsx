e1eb761b27d842dea50f6e0e0e176d47210667d3
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2023-10-28 22:01:23 -0700

    Added styles to about me section in personal page

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-import styles from '../../styles/components/personal/about.module.scss';
+import about from '../../styles/components/personal/about.module.scss';
 
 export default function About() {
     return (
-      <div>
-        <p className={styles.about} >I am a 2nd year computer science & Engineering student at UCD, who would love to utilize my learned skills to make meaningful/innovative projects that can help society! In my spare time, I love arts like crochet and baking. Looking forward to connecting with you :)</p>  
+      <div className={about.about}>
+        <div className={about.title}>
+          <p className={about.titleText}>~</p>
+          <p className={about.titleText}>my name is</p>
+          <p className={about.titleName}>Sreya Mathew</p>
+          <p className={about.titleText}>~</p>
+        </div>
+        <p className={about.summary} >I am a 2nd year Computer Science & Engineering student at UCD, who would love to utilize my learned skills to make meaningful/innovative projects that can help society! In my spare time, I love arts like crocheting and baking. Looking forward to connecting with you :)</p>  
       </div>
     )
 }
