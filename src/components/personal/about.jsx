81c36c31f2fef8ece970e0ee851eb39360fcb0c9
TheGhostCoder0 <105943310+TheGhostCoder0@users.noreply.github.com>
2023-10-30 05:51:21 -0700

    Initial profile page. Image, name, and major

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import styles from '@/styles/components/personal/about.module.scss'
 
 export default function About() {
     return (
-      <div>about</div>
+      <div className={ styles.profile }>
+        <img src='https://media.licdn.com/dms/image/C5603AQHi98RrS4B0Ww/profile-displayphoto-shrink_400_400/0/1660822747098?e=1703721600&v=beta&t=X6MWIB5NtGoG_mZmg7q8FDPDbhzTRXdFrfL48Ze1iXk'></img>
+        <h1>Keena Vasiloff</h1>
+        <h3>Computer Science</h3>
+      </div>
     )
 }
