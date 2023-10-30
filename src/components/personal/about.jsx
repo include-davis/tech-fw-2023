955ebef7c4026f9d78d39b4b561fe0d51c5a34f4
Anna Cai <anna@campus-010-145.ucdavis.edu>
2023-10-29 21:17:58 -0700

    created new branch

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-
+import styles from '@/styles/components/personal/about.module.scss';
 export default function About() {
     return (
-      <div>about</div>
+      <div className={styles.outer}>
+        about
+      </div>
     )
 }
