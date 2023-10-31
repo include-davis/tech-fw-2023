ce478ae96ed3ca1b4b2e3d6459c82839574207cf
naomi zhao <naomizhao@naomis-MacBook-Pro-3.local>
2023-10-30 22:47:15 -0700

    short info, icons, animation

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import styles from '@/styles/components/personal/about.module.scss'
+import Icons from '@/components/personal/icons'
 
 export default function About() {
     return (
-      <div className={styles.outer}>
-        content
-        <div className={styles.inner}>
-          more content
-        </div>
+      <div className={styles.style}>
+        <h1>naomi zhao</h1>
+        <h2>personal page</h2>
+        <div><text>hi, i'm naomi! i'm a class of 2026 computer science & engineering major. lorem ipsum dolor sit amet, consectetur adipiscing elit.</text></div>
+        <Icons></Icons>
       </div>
     )
 }
+
+// npm run dev
---
+++ src/components/personal/icons.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from '@/styles/components/personal/about.module.scss'
+
+export default function Icons(){
+    return(
+        <div className={styles.icons}>
+          <img src="https://stardewvalleywiki.com/mediawiki/images/6/64/Pumpkin.png"></img>
+          <img src="https://stardewvalleywiki.com/mediawiki/images/0/01/Ancient_Fruit.png"></img>
+          <img src="https://stardewvalleywiki.com/mediawiki/images/2/25/Blackberry.png"></img>
+          <img src="https://stardewvalleywiki.com/mediawiki/images/2/20/Cherry.png"></img>
+          <img src="https://stardewvalleywiki.com/mediawiki/images/6/6d/Strawberry.png"></img>
+          <img src="https://stardewvalleywiki.com/mediawiki/images/4/43/Orange.png"></img>
+          <img src="https://stardewvalleywiki.com/mediawiki/images/f/fb/Pineapple.png"></img>
+          <img src="https://stardewvalleywiki.com/mediawiki/images/c/c2/Grape.png"></img>
+          <img src="https://stardewvalleywiki.com/mediawiki/images/e/e2/Peach.png"></img>
+          <img src="https://stardewvalleywiki.com/mediawiki/images/7/7d/Apple.png"></img>
+
+        </div>
+    )
+}
