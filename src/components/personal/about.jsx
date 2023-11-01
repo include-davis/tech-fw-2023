cd8ceb92d9a9a4fdb838691932bf84cc51bfbb68
catelynbu <catelyn.bu@gmail.com>
2023-10-31 22:53:32 -0700

    update

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -4,20 +4,19 @@ import styles from '@styles/components/personal/about.module.scss'
 
 export default function About() {
     return (
-      <div className={styles.about}>
-        
-        <div>
-          <h1>Catelyn Bu About Me</h1>
+      <div className={styles.container}>
+        <div className={styles.body}>
+          
+            <h1>About Me</h1>
 
-          <h2>Hi, my name is Catelyn!</h2>
+            <h2>Hi, my name is Catelyn Bu!</h2>
 
-          <p>I am a 3rd year student at UC Davis and I am majoring in Design.</p>
+            <p>I am a 3rd year student at UC Davis and I am majoring in Design.</p>
 
-          <p>This is all very new to me and I'm still learning! I'm excited and eager to experience what has to come!</p>
+            <p>This is all very new to me and I'm still learning. I'm excited and eager to experience what has to come!</p>
+
 
         </div>
-
-
       </div>
     )
 }
