fa259b6be2277373baff21d7497fdad90726acd5
David Lee <72262475+dql1116@users.noreply.github.com>
2023-10-31 18:09:56 -0700

    started adding text to page

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -3,11 +3,31 @@ import styles from '@/styles/components/personal/about.module.scss';
 
 export default function About() {
     return (
-      <div className={styles.outer}> 
-        content
-        <div className={styles.inner}>
-          more content
+      <div className={styles.body}>
+
+        <div className={styles.header}> 
+          David Lee
+
+          <div className={styles.introduction}>
+            Hi my name is David Lee and I am from Maryland. I came to Davis
+            because of the possible connections that I could create and also because of its proximity to Silicon Valley.
+          </div>
+
+          <div className={styles.hobbies}>
+            During my free time I like to play games and hangout with friends.
+            I especially love playing and watch soccer. I have trouble waking up for class, but I somehow always wake up in time for a soccer match.
+          </div>
+
+          <div className={styles.endingParagraph}>
+            I am really excited to join #include and I can't wait for the learning opportunites and connections with people that I will make here.
+          </div>
+
+          <div className={styles.closing}>
+            Thank you!
+          </div>
+
         </div>
+
       </div>
     )
 }
