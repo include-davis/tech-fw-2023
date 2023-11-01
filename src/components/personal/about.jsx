60f9d06129bcfea9d4e92e34edd823d33b6d3d91
C3viche <crisemble@gmail.com>
2023-10-31 22:34:17 -0700

    structure of site

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import styles from "@/styles/components/personal/about.module.scss";
 
 export default function About() {
     return (
       <div>
-        Hello, my name is Cristian.
+          <style jsx global>
+            {`
+                body {
+                    background-color: rgb(238, 238, 238);
+                 }
+             `}
+          </style>
+          <body className={styles.box}>
+            <h2 className={styles.profession}>
+                <strong>UI/UX Designer</strong>
+            </h2>
+            <h1 className={styles.name}>Hello, my name is Cristian Embleton</h1>
+            <p className={styles.description}>Currently studying computer software and hardware engineering. Always looking for ways to learn more.</p>
+          </body>
       </div>
     )
 }
