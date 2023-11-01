d623ab156eb5d91cac3de791ff033abd5f8583b1
C3viche <crisemble@gmail.com>
2023-10-31 22:44:11 -0700

    html spacing bugfix

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -4,20 +4,20 @@ import styles from "@/styles/components/personal/about.module.scss";
 export default function About() {
     return (
       <div>
-          <style jsx global>
-            {`
-                body {
-                    background-color: rgb(238, 238, 238);
-                 }
-             `}
-          </style>
-          <body className={styles.box}>
-            <h2 className={styles.profession}>
-                <strong>UI/UX Designer</strong>
-            </h2>
-            <h1 className={styles.name}>Hello, my name is Cristian Embleton</h1>
-            <p className={styles.description}>Currently studying computer software and hardware engineering. Always looking for ways to learn more.</p>
-          </body>
+        <style jsx global>
+          {`
+              body {
+                  background-color: rgb(238, 238, 238);
+              }
+           `}
+        </style>
+        <div className={styles.box}>
+          <h2 className={styles.profession}>
+              <strong>UI/UX Designer</strong>
+          </h2>
+          <h1 className={styles.name}>Hello, my name is Cristian Embleton</h1>
+          <p className={styles.description}>Currently studying computer software and hardware engineering. Always looking for ways to learn more.</p>
+        </div>
       </div>
     )
 }
