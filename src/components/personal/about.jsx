d6c5d48ea73abd2bfe7a6bf1ce000515e791fcc4
kashvi <kashvi.dwarka23@gmail.com>
2023-11-01 21:47:26 -0700

    added styling to text elements - title, greeting, subgreeting, and button

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -4,11 +4,12 @@ import styles from "@/styles/components/personal/about.module.scss";
 
 export default function About() {
     return (
-      <div>
-        <div class="title">Hello, my name is Kashvi Dwarka!</div>
-        <div class="subtitle">I'm a third year student at UC Davis studying computer science. 
-            Scroll to see more about my skills and projects :D </div>
-        <button>Contact Me!</button>
+      <div className={styles.webpage}>
+        <p className={styles.job_title}>Programmer</p>
+        <h1 className={styles.greeting}>Hello, my name is Kashvi Dwarka!</h1>
+        <p className={styles.subgreeting}>I'm a third year student at UC Davis studying computer science. 
+            Scroll to see more about my skills and projects :D </p>
+        <button className={styles.contact_button}>Contact Me!</button>
       </div>
     )
 }
