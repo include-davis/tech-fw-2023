cdfebb49208949c90ddb190d0e5841088d7751a9
kashvi <kashvi.dwarka23@gmail.com>
2023-11-01 20:26:51 -0700

    attempted changes in about.module.scss but styling is not appearing on webpage

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -5,9 +5,10 @@ import styles from "@/styles/components/personal/about.module.scss";
 export default function About() {
     return (
       <div>
-        <h1>Hello, my name is Kashvi Dwarka!</h1>
-        <h3>I'm a third year student at UC Davis studying computer science. 
-            Scroll to see more about my skills and projects :D </h3>
+        <div class="title">Hello, my name is Kashvi Dwarka!</div>
+        <div class="subtitle">I'm a third year student at UC Davis studying computer science. 
+            Scroll to see more about my skills and projects :D </div>
+        <button>Contact Me!</button>
       </div>
     )
 }
