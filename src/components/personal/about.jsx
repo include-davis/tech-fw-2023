2c22160e32aa8143531633807d6a8c3571256bc8
kashvi <kashvi.dwarka23@gmail.com>
2023-11-01 23:22:43 -0700

    more styling for greeting section:

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -5,10 +5,10 @@ import styles from "@/styles/components/personal/about.module.scss";
 export default function About() {
     return (
       <div className={styles.webpage}>
-        <p className={styles.job_title}>Programmer</p>
+        <p className={styles.job_title}>Computer Science Student</p>
         <h1 className={styles.greeting}>Hello, my name is Kashvi Dwarka!</h1>
         <p className={styles.subgreeting}>I'm a third year student at UC Davis studying computer science. 
-            Scroll to see more about my skills and projects :D </p>
+            Scroll to see more about my experience, skills, and projects :D </p>
         <button className={styles.contact_button}>Contact Me!</button>
       </div>
     )
