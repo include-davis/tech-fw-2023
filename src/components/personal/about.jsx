b53b4096d5ddcbf096a150b369066fa454eaae04
kashvi <kashvi.dwarka23@gmail.com>
2023-11-01 18:59:54 -0700

    added description about myself

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -4,6 +4,10 @@ import styles from "@/styles/components/personal/about.module.scss";
 
 export default function About() {
     return (
-      <div>about</div>
+      <div>
+        <h1>Hello, my name is Kashvi Dwarka!</h1>
+        <h3>I'm a third year student at UC Davis studying computer science. 
+            Scroll to see more about my skills and projects :D </h3>
+      </div>
     )
 }
