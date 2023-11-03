6e980b487b6479d1857921cfc6ff8144c5852bf9
Nandhana Selvam <nandhanaselvam@gmail.com>
2023-11-03 16:50:15 -0700

    added padding

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -3,19 +3,27 @@ import styles from '@/styles/components/personal/about.module.scss';
 
 export default function About() {
     return (
-      <div> 
+      <div className = {styles.page}> 
+        <div className = {styles.title}>
+          Computer Science and Engineering Student
+        </div>
+
         <div className={styles.hello}>
           Hello, my name is<br>
           </br> Nandhana Selvam
         </div>
+
         <div className={styles.hello_description}>
           I am a third year Computer Science <br>
           </br>and Engineering student at UC Davis!
         </div>
 
-        <div className={styles.image}>
-          <img src="https://cdn.discordapp.com/attachments/686370685991256171/1168472608027713546/IMG_1404.jpg?ex=6551e3ea&is=653f6eea&hm=5af355af6c50e6755e0f560dc9930bbfd8f7a44016e481bcc483db9a0770d65d&" 
-            alt="cat" />
+        <p className = {styles.button}>
+          Contact Me
+        </p>
+
+        <div className={styles.filler_image}>
+          <img src="https://cdn.discordapp.com/attachments/686370685991256171/1168472608027713546/IMG_1404.jpg?ex=6551e3ea&is=653f6eea&hm=5af355af6c50e6755e0f560dc9930bbfd8f7a44016e481bcc483db9a0770d65d&"  />
         </div>
         
       </div>
--- src/pages/personal.jsx
+++ src/pages/personal.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 export default function Personal() {
   return (
     <div>
-      hello
       <About></About>
     </div>
   );
