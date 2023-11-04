a5b8328085fc2408b28a85ca5c0eaa3406138ee0
mflo20 <149274845+mflo20@users.noreply.github.com>
2023-11-03 18:29:44 -0700

    Update about.jsx

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -1,9 +1,23 @@
-import React, { Component } from "react";
-import Image from "next/image";
-import styles from "@/styles/components/personal/about.module.scss";
+import React, { Component } from 'react';
+import styles from '@/styles/components/personal/about.module.scss'; 
+
 
 export default function About() {
-  return (
-    <div>about</div>
-  );
+    return (
+      <div> 
+    <h1>Hello, my name is Martha!</h1>
+    
+    <p1>I am a fourth year, working towards double majoring in CS + Film. My goal is to become a master in web development!</p1>
+    <div><img src="/images/MarthaFloresHeadshot.jpg" width={300} height={300} alt="Martha Headshot" /></div>
+
+
+
+
+
+      </div>
+
+      
+      
+
+    );
 }
