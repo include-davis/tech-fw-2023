26972f63ec4e73f9f1c0f88f1cc605d23968cc5f
melichul <99835748+melichul@users.noreply.github.com>
2023-11-03 19:44:20 -0700

    about section

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 
 export default function About() {
     return (
-      <div>about</div>
+      <div>
+        <h3 class="intro"> 
+          hello, my name is 
+        </h3>
+
+        <h1 class="name"> 
+          emily
+        </h1> 
+
+        <p class="about_me"> 
+        I'm an undergraduate student majoring in computer science. 
+        </p>
+
+      </div>
     )
 }
