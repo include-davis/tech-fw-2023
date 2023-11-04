bd0e704457b393cf9defa3364fb0271582160c3b
Brian <108324541+FueledByOatmilk@users.noreply.github.com>
2023-11-03 17:22:58 -0700

    update

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -4,6 +4,18 @@ import styles from "@/styles/components/personal/about.module.scss";
 
 export default function About() {
   return (
-    <div>about</div>
+    <div>About Me
+
+      <br></br>
+
+      <p>Hello my name is Brian and I am a 4th year majoring in CS</p>
+
+      <hr></hr>
+
+      <p>I promise I will work on this later! </p>
+
+    </div>
+
+
   );
 }
