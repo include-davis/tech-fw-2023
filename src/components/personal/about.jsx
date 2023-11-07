9dd83b5e584e6a5eda24a206b3e192bcea4422e4
erikagosti <erika.gosti@gmail.com>
2023-11-06 16:26:40 -0800

    feat/new divs + website link

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -4,15 +4,18 @@ import styles from "@/styles/components/personal/about.module.scss";
 
 export default function About() {
   return (
-    <div>
-      <div className="title">
-        hi, i'm erika!
-      </div>
-      <div>
+    <div className={styles.text}>
+      <element className={styles.title}>
+        hello, i'm erika!
+      </element>
+      <div className={styles.description}>
         i'm a first-year compsci student. i love
         dance and all things artsy. i also really
         like sanrio, especially kuromi ...
       </div>
+      <a href="https://www.erikagosti.com" className={styles.website}>
+        check out my website --&#62;
+      </a>
     </div>
   );
 }
