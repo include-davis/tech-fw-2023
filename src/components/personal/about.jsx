657fdc83ed8c9eb3b16ed3fa3eddbd9b6698b877
C3viche <crisemble@gmail.com>
2023-11-07 22:55:59 -0800

    changed description and sizing a bit

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -5,34 +5,34 @@ import Image from "next/image";
 
 export default function About() {
     return (
-      <div>
+      <section>
         <nav className={styles.nav}>
           <div className={styles.nav_menu}><AiOutlineMenu size={28}/></div>
         </nav>
         <div className={styles.body}>
           <div className={styles.about}>
             <h2 className={styles.profession}>UI/UX Designer</h2>
-            <h1>Hello, my name is Cristian Embleton</h1>
-            <p className={styles.description}>Currently studying computer software and hardware engineering. Always looking for ways to learn more.</p>
+            <h1 className={styles.name}>Cristian Embleton</h1>
+            <p className={styles.description}>Currently studying computer software and hardware engineering at UC Davis. Always looking for ways to learn more.</p>
             <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0">
               <button type="button" className={styles.button}>Contact me</button>
             </a>
           </div>
           <div className={styles.picture}>
-            <Image className={styles.image_bg}
+            {/* <Image className={styles.image_bg}
               src="/purple_circle.png"
               width={100}
               height={100}
               alt={"backdrop"}
-            />
+            /> */}
             <Image className={styles.image}
               src="/cristian.png"
               width={250}
-              height={241}
+              height={250}
               alt={"portfolio picture"}
             />
           </div>
         </div>
-      </div>
+      </section>
     )
 }
