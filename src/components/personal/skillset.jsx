49231b837197bf8c6a39cb66e11532d96ae7ee8a
C3viche <crisemble@gmail.com>
2023-11-08 21:43:20 -0800

    added skillset section below about

--- src/components/personal/skillset.jsx
+++ src/components/personal/skillset.jsx
@@ -1,5 +1,9 @@
 // import styles from styling folder!
 import styles from "@/styles/components/personal/skillset.module.scss"
+import { TbBrandCpp } from 'react-icons/tb'
+import { SiJulia } from 'react-icons/si'
+import { BiLogoJava } from 'react-icons/bi'
+
 
 /* Think about what you want to include for skills
     1. Some ideas might be coding languages, frameworks, libraries, etc
@@ -9,22 +13,40 @@ import styles from "@/styles/components/personal/skillset.module.scss"
 
 export default function Skills() {
   return  (
-    <section className={styles.container}>
-      <div className={styles.card}>
-        <h4 className={styles.skills}>C++</h4>
-        <h4 className={styles.skills}>Julia</h4>
-        <h4 className={styles.skills}>HTML5/CSS</h4>
+    <section className={styles.professional}>
+      <h1 className={styles.title}>Professional Development</h1>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <TbBrandCpp size={48}/>
+          <SiJulia size={48}/>
+          <BiLogoJava size={48}/>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.club}>
+            <h2>#include</h2>
+            <p>Student-run consulting agency dedicated to building digital solutions for the community</p>
+          </div>
+          <div className={styles.club}>
+            <h2>AISC</h2>
+            <p>A club to network and learn about artificial intelligence and machine learning</p>
+          </div>
+        </div> 
+        <div className={styles.card}>
+          <div className={styles.project}>
+            <h4>NixOS Flake</h4>
+            <p>A customizable, declarative, and portable desktop environment</p>
+          </div>
+          <div className={styles.project}>
+            <h4>Knight's Tour</h4>
+            <p>Calculates the path of a knight such that it visits every square once</p>
+          </div>
+          <div className={styles.project}>
+            <h4>Minesweeper</h4>
+            <p>Full scale Minesweeper clone with custom graphics and extra features</p>
+          </div>
+        </div>
       </div>
-      <div className={styles.card}>
-        <h4 className={styles.skills}>C++</h4>
-        <h4 className={styles.skills}>Julia</h4>
-        <h4 className={styles.skills}>HTML5/CSS</h4>
-      </div> 
-      <div className={styles.card}>
-        <h4 className={styles.skills}>C++</h4>
-        <h4 className={styles.skills}>Julia</h4>
-        <h4 className={styles.skills}>HTML5/CSS</h4>
-      </div>
+      
     </section>
   )
 }
