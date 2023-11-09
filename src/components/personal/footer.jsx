f89e4dfeccd3473383b60c941781481e14810138
naomi zhao <naomizhao@campus-011-211.ucdavis.edu>
2023-11-09 12:04:28 -0800

    html footer

--- src/components/personal/footer.jsx
+++ src/components/personal/footer.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import styles from '@/styles/components/personal/about.module.scss'
-import { FiMail } from 'react-icons/fi';
+import { FiMail, FiInstagram } from 'react-icons/fi';
 
-export default function Footer(){
-    return(
-        <div className={styles.footer}>
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <section className={styles.logo}>logo</section>
+
+            <section className={styles.info}>
+                <p>made by naomi</p>
+                <ul className={styles.footerIcons}>
+                    <li> <a href="mailto:naomitan.zhao@gmail.com"><FiMail/></a> </li>
+                    <li> <a href="https://www.instagram.com/naomi_zhzz/"><FiInstagram/></a> </li>
+                </ul>
+            </section>
+        </footer>
+    );
+}
+
+/* <div className={styles.footer}>
             <div className={styles.footerIcons}>
                 <FiMail/>
             </div>
             
-        </div>
-    )
-}
+        </div> */
