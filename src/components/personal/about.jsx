2ba1a7e5bfb1e45e7ca25488602bc7921a9773ca
Brian <108324541+FueledByOatmilk@users.noreply.github.com>
2023-11-09 13:55:05 -0800

    Update 2

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -4,18 +4,27 @@ import styles from "@/styles/components/personal/about.module.scss";
 
 export default function About() {
   return (
-    <div>About Me
+    <div className={styles.style}>
 
+    <h1>Brian Dean Nguyen</h1>
+    <br></br>
+    <h2>Welcome to My Personal Page!</h2>
+
+    <div>
+      <p>Hello, My name is Brian. I am a fourth year transfer student majoring in Computer
+        Science and minoring in Tech management. On my free time I like to spend time with
+        friends, go on long walks and take spontaeous trips. My hobbies are cooking, reading,
+        Boxing, MMA and reading. Fun fact about me is, I go to the gym everyday to lift weights
+        but I don't necessarily like it. I have a dog named Butters.
+      </p>
       <br></br>
-
-      <p>Hello my name is Brian and I am a 4th year majoring in CS</p>
-
-      <hr></hr>
-
-      <p>I promise I will work on this later! </p>
-
+      <text>Looking forward to meeting all of you!</text>
     </div>
 
-
-  );
+    </div>
+  )
 }
+
+//npm install
+//npm run dev
+//localhost/personal
