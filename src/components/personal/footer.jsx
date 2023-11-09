02b25d47e4072e006e2fa190d5629d2bdf910a50
C3viche <crisemble@gmail.com>
2023-11-09 10:36:12 -0800

    basic footer with links to github and linkedin

--- src/components/personal/footer.jsx
+++ src/components/personal/footer.jsx
@@ -1,6 +1,8 @@
 // import SCSS file as styles for this component
 import styles from "@/styles/components/personal/footer.module.scss";
 import Image from "next/image";
+import { AiFillGithub } from 'react-icons/ai'
+import { AiFillLinkedin } from 'react-icons/ai'
 
 export default function Footer() {
   // Make a footer!
@@ -19,21 +21,20 @@ export default function Footer() {
   */
   return (
     <footer className={styles.footer}>
-      <hr/>
-      <section className={styles.logo}>logo</section>
-
-      <section className={styles.info}>
-        <p>Made by #your name here#</p>
-        <ul className={styles.socials}>
-          <li className={styles.icon}> SOCIAL MEDIA ICONS + REDIRECTING </li>
-          <Image
-              src="/favicon-32x32.png"
-              width={30}
-              height={30}
-              alt={"portfolio picture"}
-            />
-        </ul>
-      </section>
+      <a href="https://github.com/C3viche" target="_blank" className={styles.socials}>
+        <AiFillGithub size={48}/>
+      </a>
+      <a href="https://includedavis.com" target="_blank" className={styles.socials}>
+        <Image
+          src="/favicon-32x32.png"
+          width={30}
+          height={30}
+          alt={"#include icon"}
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/cristian-embleton/" target="_blank" className={styles.socials}>
+        <AiFillLinkedin size={48}/>
+      </a>
     </footer>
   );
 }
