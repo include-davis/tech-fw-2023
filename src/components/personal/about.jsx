f81b5ec4ad1c69e0c0f8579eceb019b8e7856fb6
kashvi <kashvi.dwarka23@gmail.com>
2023-11-09 18:10:02 -0800

    updated About section - moved to using flexbox

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -4,18 +4,21 @@ import styles from "@/styles/components/personal/about.module.scss";
 
 export default function About() {
     return (
-      <div className={styles.webpage}>  {/*container for entire webpage*/}
+      <div className={styles.about_me}>  {/*container for entire about me section*/}
         <div className={styles.about_text}> {/*container for about me text*/}
+          {/* about_text items*/}
           <p className={styles.job_title}>Computer Science Student</p>
           <h1 className={styles.greeting}>Hello, my name is Kashvi Dwarka!</h1>
           <p className={styles.subgreeting}>I'm a third year student at UC Davis studying computer science. 
               Scroll to see more about my experience, skills, and projects :D </p>
           <button className={styles.contact_button}>Contact Me!</button>
-        </div>  {/*end container for about me text*/}
-          <div className={styles.personalImage}>
-            <Image src="/personalImage.jpg" width={350} height={450} alt="personal image"/> {/*filler image for now - need to find good picture of myself lol*/}
-          </div>
-        {/*end container for entire webpage*/}
+          {/*end container for about me text*/}
+        </div>  
+
+        <div className={styles.personalImage} style={{ display: 'flex', justifyContent: 'center', alignItems: 'start' }}>
+          <Image src="/personalImage.jpg" width={350} height={450} alt="personal image" />
+        </div>
+        {/*end container for about me section*/}
       </div> 
     )
 }
