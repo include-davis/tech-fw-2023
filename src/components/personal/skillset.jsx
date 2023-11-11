a441e2ced0601d573374e427ab36490a700adc46
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2023-11-11 13:52:35 -0800

    Added skills section!!

--- src/components/personal/skillset.jsx
+++ src/components/personal/skillset.jsx
@@ -1,4 +1,5 @@
 // import styles from styling folder!
+import styles from "../../styles/components/personal/skillset.module.scss";
 
 /* Think about what you want to include for skills
     1. Some ideas might be coding languages, frameworks, libraries, etc
@@ -6,6 +7,23 @@
     3. Could also put in some of your hobbies or interests
 */
 
+
 export default function Skills() {
-  return <section className={styles.skills}>skills</section>;
+  return <section className={styles.skills}>
+    <h1 className={styles.skills_title}> SKILLS </h1>
+    
+    <div className={styles.skills_container}>
+      <div className={styles.skills_set}>
+        <h1 className={styles.set_title}>languages = [</h1>
+        <p>C++,<br></br>Javascript,<br></br></p>
+        <h1 className={styles.set_title}>]</h1>
+      </div>
+
+      <div className={styles.skills_set}>
+        <h1 className={styles.set_title}>soft_skills = [</h1>
+        <p>Planning,<br></br>Communication,<br></br></p>
+        <h1 className={styles.set_title}>]</h1>
+      </div>
+    </div>
+  </section>;
 }
