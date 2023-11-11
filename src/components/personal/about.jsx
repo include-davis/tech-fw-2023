23936ab2d107d7436afd077f77701e76f843007a
Krigupt <123670605+Krigupt@users.noreply.github.com>
2023-11-10 20:46:30 -0800

    task2

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -1,24 +1,12 @@
-import React, { Component } from "react";
-import styles from "@/styles/components/personal/about.module.scss";
-import Image from "next/image";
-//i explain the use of this in a later comment^
-
-// import styles from '/Users/krishna/Desktop/untitled folder/tech-fw-2023/src/styles/components/personal/about.module.scss';
-//we are covering imports and paths in this week's workshop so no worries if you couldn't get the path right! The '@' symbol is used to indicated that you are starting off in 'src' which is what we defined in the jsconfig file
-
-//Some general pointers:
-//Great use of utilizing classNames! It is important, however, to use meaningful names. For example, instead of 'h', you can try using 'job_title'.
-
-//I noticed you used <br> in your text. This is okay, but I think you just wanted to wrap your text at a certain point. Instead of <br>, you can add a width to the container holding your text in your css file. That way, everything can wrap and be aligned nicely.
+import React from 'react';
+import styles from '@/styles/components/personal/about.module.scss'; // Adjust the path based on your project structure
 
 export default function About() {
   return (
-    <div>
+    <div className={styles.bg}>
       <div>
-        {/* You identified a great way to split up the text, so good job! A suggestion would be to move all of your <p> elements into the same <div> (so move .font1 and .box) here and give the div a className along the lines of "text" or something like that. */}
         <span className={styles.h}>
           <strong>UI/UX DESIGNER</strong>
-          {/* <strong> definitely works. Instead I would say you can use the font-weight property in your CSS if you want bold text. <strong> is usually used for elements of high importance like warnings and some screen readers will also recognize these tags and say them differently. */}
         </span>
       </div>
 
@@ -37,11 +25,42 @@ export default function About() {
       </p>
 
       <p className={styles.box}>Contact Me</p>
-      {/* Since contact me is a button, i would suggest using the <button> tag instead */}
 
       <div className={styles.pic}>
         <img src="/pic.jpeg" alt="Description" />
-        {/* Since we are using Next.js, you can actually import the Image component instead. It's optimized, so we'll use the Image component instead in the future. This wasn't covered, so no worries! You used <img> perfectly!*/}
+        <p>
+          I am very passionate about web development,<br />
+          love to do projects related to it,
+          <br />
+          and I am also interested in computer vision, <br />
+          and the application of AI/ML in different fields.
+        </p>
+      </div>
+
+
+      <div className={styles.skill}>
+        <strong>Skills</strong>
+      </div>
+
+      <div className={styles.content_box}>
+        <div className={styles.pic1}>
+          <img src="/CSS.png" alt="Description" />
+        </div>
+        <div className={styles.pic2}>
+          <img src="/flask.png" alt="Description" />
+        </div>
+        <div className={styles.pic3}>
+          <img src="/HTML.png" alt="Description" />
+        </div>
+        <div className={styles.pic4}>
+          <img src="/Javascript.png" alt="Description" />
+        </div>
+        <div className={styles.pic5}>
+          <img src="/next.png" alt="Description" />
+        </div>
+        <div className={styles.pic6}>
+          <img src="/react.jpeg" alt="Description" />
+        </div>
       </div>
     </div>
   );
