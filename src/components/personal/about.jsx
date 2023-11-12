d205ba77553532cd53fb750809935445b61c2c1d
Ryan <ryyu444@gmail.com>
2023-11-11 18:38:25 -0800

    Moved about example to separate component in week 2

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -1,54 +1,10 @@
 import React, { Component } from "react";
-import Image from "next/image";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { TiSocialInstagramCircular } from "react-icons/ti";
-import { SiGmail } from "react-icons/si";
 import styles from "@/styles/components/personal/about.module.scss";
 
 export default function About() {
   return (
     <div className={styles.about}>
-      <div className={styles.infoCard}>
-        <div className={styles.infoCardText}>
-          <h1>Welcome! 👋</h1>
-          <p>My name is Ryan and this is my portfolio. 😎</p>
-        </div>
-        <div className={styles.socials}>
-          <div className={styles.logos}>
-            <div className={styles.logoGroup}>
-              <a href={""} target="_blank">
-                <FaLinkedin className={styles.logo} />
-              </a>
-              <a href={""} target="_blank">
-                <FaGithub className={styles.logo} />
-              </a>
-            </div>
-            <div className={styles.logoGroup}>
-              <a href={""} target="_blank">
-                <TiSocialInstagramCircular className={styles.logo} />
-              </a>
-              <a href={""} target="_blank">
-                <SiGmail className={styles.logo} />
-              </a>
-            </div>
-          </div>
-          <div className={styles.buttons}>
-            <button className={styles.projectButton}>View Projects</button>
-            <button className={styles.connectButton}>Get In Touch</button>
-          </div>
-        </div>
-      </div>
-      <div className={styles.aboutMe}>
-        <h1>About Me</h1>
-        <div className={styles.aboutMeText}>
-          <p>I'm a 3rd year CS Major who started off with Economics</p>
-          <p>
-            I love all things green, especially <span>Matcha</span> 🍵
-          </p>
-          <p>Gib more plushes {"(>._.)>"} 🧸</p>
-        </div>
-      </div>
+      <h1>hello</h1>
     </div>
   );
 }
