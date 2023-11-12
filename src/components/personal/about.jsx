2ebfe967bc2ddd57733d17fe14dc1eed7c558632
catelynbu <catelyn.bu@gmail.com>
2023-11-11 19:47:30 -0800

    css fixes + added buttons on about

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -6,27 +6,31 @@ export default function About() {
     return (
       <div className={styles.container}>
         <div className={styles.body}>
-          
+
+            <img className={styles.pompom} src={"/personal/pompom.png"} alt={"pompompurin"}/>
+
+          <div className={styles.textContainer}>
             <h1>About Me</h1>
 
             <h2>Hi, my name is Catelyn Bu!</h2>
 
-            <img className={styles.pompom} src={"/personal/pompom.png"} alt={"pompompurin"}/>
 
             <p>I am a 3rd year student at UC Davis and I am a Design major. I'm excited to participate in Include and to experience what has to come!</p>
 
-            <p>I've also done previous basic website builds which I'll link here:</p>
-              <ul>
-                <li>
-                  <a href="https://appliedartand.design/85-OL-sp23/Catelyn-Bu/" target="_blank">Catelyn Bu Portal Page</a>
-                </li>
+            <p>I've also done previous basic website builds which are linked below.</p>
 
-                <li>
-                  <a href="https://appliedartand.design/85-OL-sp23/Catelyn-Bu/catelyn-Bu-Final-root/" target="_blank">Catelyn Bu Art & Design Portfolio</a>
-                </li>
-              </ul>
-
+            <div className={styles.buttons}>
+                <a href="https://appliedartand.design/85-OL-sp23/Catelyn-Bu/" target="_blank">
+                  <button className={styles.buttonPortal}>Catelyn Bu Portal Page</button>
+                </a>
+                <a href="https://appliedartand.design/85-OL-sp23/Catelyn-Bu/catelyn-Bu-Final-root/" target="_blank">
+                  <button className={styles.buttonPortfolio}>Catelyn Bu Art & Design Portfolio</button>
+                </a>
+            </div>
+          </div>
         </div>
+
+
       </div>
     )
 }
