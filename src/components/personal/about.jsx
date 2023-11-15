61815403b31b4d03cc9ca63fac3f4ef8fb199e17
TheGhostCoder0 <105943310+TheGhostCoder0@users.noreply.github.com>
2023-11-14 16:35:26 -0800

    Added personal webpage to page

--- src/components/personal/about.jsx
+++ src/components/personal/about.jsx
@@ -11,6 +11,9 @@ export default function About() {
           <h1>Keena Vasiloff</h1>
           <h3>Computer Science</h3>
           <h5>I love to write code and build projects.</h5>
+          <a href={"https://theghostcoder0.github.io/PersonalPortfolio/"} target="_blank">
+            <button>View My Personal Website </button>
+          </a>
         </div>
         <div className={styles.photo}>
           <img src='https://media.licdn.com/dms/image/C5603AQHi98RrS4B0Ww/profile-displayphoto-shrink_400_400/0/1660822747098?e=1703721600&v=beta&t=X6MWIB5NtGoG_mZmg7q8FDPDbhzTRXdFrfL48Ze1iXk' alt='Keena Vasiloff'></img>
