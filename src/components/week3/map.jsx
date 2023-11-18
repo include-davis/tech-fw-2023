2e2aaac39fe8365736f19d5d12424f208a290d7c
C3viche <crisemble@gmail.com>
2023-11-17 19:15:29 -0800

    workshop 4

--- src/components/week3/map.jsx
+++ src/components/week3/map.jsx
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 import styles from "@/styles/components/week3/map.module.scss";
 //create an array with 3-4 of your favorite things here
 
+const myFavThings = ["running", "rice", "dogs", "chocolate", "music"]
+
 export default function Map() {
   //create an arrow function mapping your favorite things to list items here
+
+  const list = (array) => {
+    return (array.map((elem) => <li>{elem}</li>))
+  }
 
   return (
     <section className={styles.container}>
       <h1 className={styles.header}>My Favorite Things Are: </h1>
+      <ul>{list(myFavThings)}</ul>
       {/* create an unordered list that calls on our list function */}
     </section>
   );
