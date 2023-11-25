775f3add0950e0a87a7cf08a6fb5def3c8a1b8ef
kashvi <kashvi.dwarka23@gmail.com>
2023-11-24 16:22:34 -0800

    added files for navbar

--- src/pages/personal.jsx
+++ src/pages/personal.jsx
@@ -1,11 +1,13 @@
-import Skills from '@/components/personal/skillset';
-import About from '@/components/personal/about';
-import Footer from '@/components/personal/footer';
-import React from 'react';
+import Skills from "@/components/personal/skillset";
+import Navbar from "@/components/personal/navbar";
+import About from "@/components/personal/about";
+import Footer from "@/components/personal/footer";
+import React from "react";
 
 export default function Personal() {
   return (
     <div>
+      <Navbar />
       <About />
       <Skills />
       <Footer />
