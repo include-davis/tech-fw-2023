d2f4fb3e2d05c11c877d3372f2dff7a2da495051
kashvi <kashvi.dwarka23@gmail.com>
2023-11-25 23:36:51 -0800

    working on implementing map function

--- src/components/personal/navbar.jsx
+++ src/components/personal/navbar.jsx
@@ -1,7 +1,38 @@
-import React, { Component } from "react";
-import Image from "next/image";
-import styles from "@/styles/components/personal/about.module.scss";
-// I am a huge fan of how you laid everything out, really good work! The only thing I wanted to point out is to move the styling for your image div into the CSS file. Some elements might require in-line but when possible, we want external CSS Also, if you go with my suggestion (in CSS file) to move the image to the right side, you can remove the personalImage div altogether. Other than that, you have good classNames and good format :)
+import React, { Component, useState } from "react";
+import styles from "@/styles/components/personal/navbar.module.scss";
+import Link from "next/link";
+
+const nav = [
+  { link: "Home", href: "" },
+  { link: "Skills", href: "" },
+  { link: "Projects", href: "" },
+  { link: "Contact Me", href: "" },
+];
+
+const dropDown_items = ["CowChow Rewards", "Connect N", "Rock Paper Scissors"];
+
 export default function Navbar() {
-  return <div></div>;
+  // const [active, setActive] = useState(false);
+
+  const list = (array) => {
+    return array.map((elem) => <li>{elem}</li>);
+  };
+
+  const dropDown = () => {
+    return (
+      <ul className={styles.dropDown}>
+        {dropDown_items.map((numWeek, index) => (
+          <Link href={`/week/${numWeek}`} key={`Week ${index}`}>
+            <li>Week {numWeek}</li>
+          </Link>
+        ))}
+      </ul>
+    );
+  };
+  return (
+    /* styles.navbar = div for entire navbar */
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>KASHVI'S PORTFOLIO</div>
+    </nav>
+  );
 }
