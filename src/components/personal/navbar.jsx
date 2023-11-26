e9247140b740b07c849adaab8ba2bc2adaed2c71
kashvi <kashvi.dwarka23@gmail.com>
2023-11-26 02:17:19 -0800

    changed color scheme

--- src/components/personal/navbar.jsx
+++ src/components/personal/navbar.jsx
@@ -2,6 +2,12 @@ import React, { Component, useState } from "react";
 import styles from "@/styles/components/personal/navbar.module.scss";
 import Link from "next/link";
 import Skills from "@/components/personal/skillset";
+
+<link
+  href="https://fonts.googleapis.com/css2?family=Poppins&family=Trispace:wght@300&display=swap"
+  rel="stylesheet"
+></link>;
+
 /*
 const nav = [
   { link: "Home", href: "" },
@@ -17,8 +23,10 @@ const dropDown_items = [
   { link: "More", href: "" },
 ];
 
+// DOCUMENTATION: https://legacy.reactjs.org/docs/events.html#mouse-events
+
 export default function Navbar() {
-  //const [isDropdownOpen, openDropdown] = useState(false); //TODO - original attempt did not work LOL
+  const [isDropdownOpen, openDropdown] = useState(false); //TODO - original attempt did not work LOL
   // use useState for dropdown menu - will appear under Projects navtitle
 
   // based off useState workshop slides + navbar src code
@@ -32,18 +40,23 @@ export default function Navbar() {
       <ul className={styles.nav_titles}>
         <li className={styles.links}>Home</li>
         <li className={styles.links}>Skills</li>
-        <li className={styles.links}>
+        <li
+          className={styles.links}
+          onMouseEnter={toggleDropdown}
+          onMouseLeave={closeDropdown}
+        >
           Projects
-          {/*{dropDown_items.map((title) => {
-            return (
-              <Link
-                href={`/${title.href.toLowerCase()}`}
-                className={styles.link}
-              >
-                {title.link}
-              </Link>
-            );
-          })}{" "} */}
+          {isDropdownOpen && (
+            <ul className={styles.dropdown}>
+              {dropDown_items.map((title) => (
+                <li className={styles.dropdownItem}>
+                  <Link href={`/${title.href.toLowerCase()}`}>
+                    {title.link}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
         </li>
         <li className={styles.links}>Contact Me</li>
       </ul>
