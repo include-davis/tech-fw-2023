333a4427d2c0b0080a8b5a53c973d06d75fd65ec
erikagosti <erika.gosti@gmail.com>
2023-11-27 21:27:47 -0800

    updated navbar

--- src/pages/personal.jsx
+++ src/pages/personal.jsx
@@ -1,11 +1,21 @@
 import Skills from '@/components/personal/skillset';
 import About from '@/components/personal/about';
+import Navbar from '@/components/personal/navbar';
 import Footer from '@/components/personal/footer';
 import React from 'react';
+
+// ordered; write in order as you want them to appear
+const navLinks = [
+  {name: "Home", href: "/home"}, 
+  {name: "About", href: "/about"}, 
+  {name: "Contact", href: "/contact"}, 
+  {name: "Services", href: "/services"}
+]
 
 export default function Personal() {
   return (
     <div>
+      <Navbar links={navLinks}/>
       <About />
       <Skills />
       <Footer />
