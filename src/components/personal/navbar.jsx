bd1de4de85ab6af1b104d19fe5555433b6fbcbe1
TheGhostCoder0 <105943310+TheGhostCoder0@users.noreply.github.com>
2023-12-03 13:50:35 -0800

    Initialization of navbar

---
+++ src/components/personal/navbar.jsx
@@ -0,0 +1,5 @@
+import styles from '@/styles/components/personal/navbar.module.scss';
+
+export default function Navbar() {
+    return (<nav>Navbar</nav>)
+}
--- src/pages/personal.jsx
+++ src/pages/personal.jsx
@@ -1,11 +1,13 @@
 import Skills from '@/components/personal/skillset';
 import About from '@/components/personal/about';
 import Footer from '@/components/personal/footer';
+import Navbar from '@/components/personal/navbar';
 import React from 'react';
 
 export default function Personal() {
   return (
     <div>
+      <Navbar />
       <About />
       <Skills  />
       <Footer />
