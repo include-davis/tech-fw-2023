850521ab654b63cad8ad0a2d926386a4c535406f
TheGhostCoder0 <105943310+TheGhostCoder0@users.noreply.github.com>
2023-12-03 16:03:10 -0800

    Navbar first two links and logo

--- src/components/personal/navbar.jsx
+++ src/components/personal/navbar.jsx
@@ -1,5 +1,17 @@
 import styles from '@/styles/components/personal/navbar.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Navbar() {
-    return (<nav>Navbar</nav>)
+    return (
+        <nav className={styles.nav}>
+            <Link href={'https://includedavis.com/'}>
+                <Image className={styles.logo} width={40} height={40} src={'/personal/logo.png'}/>
+            </Link>
+            <div className={styles.links}>
+                <Link className={styles.link} href={'https://www.keenavasiloff.com/'} target='_blank'>Personal Website</Link>
+                <Link className={styles.link} href={'https://www.keenavasiloff.com/projects.html'}>Projects</Link>
+            </div>
+        </nav>
+    )
 }
