0e31396ed181876d133e1de713ba5472fb514d09
C3viche <crisemble@gmail.com>
2023-12-03 16:34:28 -0800

    add dummy pages

---
+++ src/pages/help.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import My_Navbar from "../components/personal/my-navbar"
+
+export default function Help() {
+  return(
+    <div>
+      <My_Navbar />
+      <h1>
+        Nothing here quite yet.
+      </h1>
+    </div>
+  )
+}
---
+++ src/pages/services.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import My_Navbar from "../components/personal/my-navbar"
+
+export default function Services() {
+  return(
+    <div>
+      <My_Navbar />
+      <h1>
+        Not good enough to be offering any services yet.
+      </h1>
+    </div>
+  )
+}
