3cd224084c47b28bba6e7a9f3ca8735e9fa5ee94
David Lee <72262475+dql1116@users.noreply.github.com>
2023-12-08 18:56:22 -0800

    made some what of a navbar

--- src/pages/personal.jsx
+++ src/pages/personal.jsx
@@ -1,11 +1,20 @@
 import Skills from '@/components/personal/skillset';
 import About from '@/components/personal/about';
+import Navbar from '@/components/personal/navbar';
 import Footer from '@/components/personal/footer';
 import React from 'react';
+
+const navLinks = [
+  {name: "Home", href: "/home"},
+  {name: "Services", href: "/services"},
+  {name: "About", href: "/about"},
+  {name: "Contact", href: "/contact"}
+]
 
 export default function Personal() {
   return (
     <div>
+      <Navbar links={navLinks}/>
       <About />
       <Skills  />
       <Footer />
