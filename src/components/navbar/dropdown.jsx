10eddc937ac51ffe9f61dd529549fb2c4d549533
JayJ104 <123anjalijain@gmail.com>
2023-12-10 16:43:26 -0800

    added dropdown in skillset as an example
    I made changes in dropdown.jsx and skillset.jsx. You can revert these changes and do your own thing after looking at the example.

--- src/components/navbar/dropdown.jsx
+++ src/components/navbar/dropdown.jsx
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 import Image from "next/image"; 
 import {useState} from 'react';
 
-import styles from "@/styles/components/personal/dropdown.module.scss";
+// Jay: uncomment the following import statement once you've moved your dropdown.module.scss into the personal folder. 
+// It won't work right now since the file is in navbar folder instead.
+// import styles from "@/styles/components/personal/dropdown.module.scss";
+
+// delete this import statement once you move the file.
+import styles from "@/styles/components/navbar/dropdown.module.scss";
 
 const myFavThings = ["scooby", "chcolates", "cookies"];
 const myotherthings = ["smth1", "smth2"]
 
+// Jay: also, it is convention to capitalize the first letter of a component.
+// React gets super confused otherwise.
 
-
-export default function dropdown() {
+export default function Dropdown() {
     const list = (array) => {
         return (array.map((elem) => <li>{elem}</li>));
     }
--- src/components/personal/skillset.jsx
+++ src/components/personal/skillset.jsx
@@ -1,6 +1,7 @@
 // import styles from styling folder!
 import styles from "@/styles/components/personal/skillset.module.scss";
-
+// Jay: this is the import statement you want to use wherever you want your dropdown. This is just an example. 
+import Dropdown from "../navbar/dropdown";
 
 /* Think about what you want to include for skills
     1. Some ideas might be coding languages, frameworks, libraries, etc
@@ -11,6 +12,8 @@ import styles from "@/styles/components/personal/skillset.module.scss";
 export default function Skills() {
   return (
   <section className={styles.skills}>
+    {/* Jay: remove the dropdown tag once you see understand how this is done. */}
+    <Dropdown/>
     <h1 className={styles.header}>Skills</h1>
       
     <h2 className={styles.header1}>HTML</h2>
