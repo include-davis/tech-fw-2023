defa19c3325cf26a40a7b3fdb123033dda83b571
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2024-01-14 22:43:53 -0800

    Made it more aesthetic! :)

--- src/pages/personal.jsx
+++ src/pages/personal.jsx
@@ -3,10 +3,11 @@ import About from '@/components/personal/about';
 import Footer from '@/components/personal/footer';
 import React from 'react';
 import NavBar_Personal from '@/components/personal/navbar';
+import personal from "../styles/components/personal/personal.module.scss";
 
 export default function Personal() {
   return (
-    <div>
+    <div className={personal.page}>
       <NavBar_Personal />
       <About />
       <Skills />
